12e412bdeb8da0addcbe0d4a68f9a2723a4342af
Foxdev <thanhduynt2604@gmail.com>
2025-07-03 14:11:45 +0700

    init project

---
+++ src/test/java/com/foxdev/project/findJobProject/FindJobProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foxdev.project.findJobProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindJobProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
